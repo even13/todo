edfd1de6a8dfad19df7572ac03339420d3ba7d12
Eve Noirault <eve.noirault@live.fr>
2021-01-11 11:44:32 +0000

    Feat: Mark item as complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 #rake routes is deprecated on rails 6.1. Use rails routes instead to see routes
-  resources :todo_lists do
-    resources :todo_items
-   end
 
-  root "todo_lists#index"
+resources :todo_lists do
+  resources :todo_items do
+    member do
+      patch :complete
+    end
+  end
+end
+
+root "todo_lists#index"
 
 end
